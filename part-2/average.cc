7f67135cb20d1da029dbb047f01fbddf075c446c
BradleyB30 <143047175+BradleyB30@users.noreply.github.com>
2023-10-30 17:04:44 -0700

    Update average.cc

--- part-2/average.cc
+++ part-2/average.cc
@@ -9,7 +9,21 @@ int main(int argc, char* argv[]) {
 
   // TODO: Validate that there is at least one command line argument.
   // If not, print an error message and return a non-zero value.
-
+  if (static_cast<int>(arguments.size())< 2) {
+  std::cout << " error: you must supply at least one number \n";
+  return 1;
+  }
+  double total{0.0};
+  bool first{true};
+  for(std::string argument : arguments) {
+   float numbers = std::stod(argument,nullptr);
+   if(first == true) {
+  first = false;
+   } else {
+   total += numbers;
+   }
+   
+  }
   // TODO: Write a for-each loop to sum (add up) all of the command line
   // arguments.
   // Use a double or float type so that your program preserves fractional
@@ -18,7 +32,10 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
+  
+  double average{0.0};
 
+  average = total / (arguments.size() - 1);
   // TODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
@@ -26,6 +43,7 @@ int main(int argc, char* argv[]) {
   // TODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
+  std::cout << "average = " << average;
 
   return 0;
 }
